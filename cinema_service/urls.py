b90fba74cad2ea7fc2f2f1d056544998d00a56b2
Andriy <andrijbuluk@gmail.com>
2026-06-11 17:28:03 +0200

    Fix: Debug toolbar start only when DEBUG=True

--- cinema_service/urls.py
+++ cinema_service/urls.py
@@ -1,9 +1,16 @@
 import debug_toolbar
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("cinema.urls")),
-    path("__debug__/", include(debug_toolbar.urls)),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns += [
+        path("__debug__/", include(debug_toolbar.urls)),
+    ]
